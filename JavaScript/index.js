b567e2e051d626a1d36aa95333f4b4d7052c860e
Roberto Carraretto <rc.carraretto@gmail.com>
2020-08-02 17:34:35 +0200

    refactor skill list
    Extract function

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,311 +5,6 @@ const detail = document.getElementById('skill-detail');
 
 
 //skills description javascript
-//HTML
-
-const html = document.getElementById('HTML');
-
-html.addEventListener ('mouseenter', function () {
-  skill_description (
-    'HTML', 
-    'このサイト程度であれば制作できます。気づけばdivだらけになっている気がしますが、、、このサイトはテンプレート等一切使わず０から全部自分で記述しました。'
-  );
-});
-
-html.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//CSS
-
-const css = document.getElementById('CSS');
-
-css.addEventListener ('mouseenter', function () {
-  skill_description (
-    'CSS', 
-    '簡単なWebサイト程度であれば制作できます。このサイトはテンプレート等一切使わず０から全部自分で記述しました。ただしまだ未熟者の為、スパゲティコードになりかけてます。'
-  );
-});
-
-css.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Bootstrap
-
-const bootstrap = document.getElementById('Bootstrap');
-
-bootstrap.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Bootsrtap', 
-    'CSSに恐怖心を抱いていた頃は非常にお世話になりました。後述のRailsをつかった自作アプリ全てにおいて使用しております。'
-  );
-});
-
-bootstrap.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-//JS
-
-const js = document.getElementById('JS');
-
-js.addEventListener ('mouseenter', function () {
-  skill_description (
-    'JavaScript', 
-    '後述の自作アプリで、Google Map APIを使いこなす必要があったため急遽一週間で詰め込み勉強しました。見よう見まねでRailsにjQueryで簡単なAjaxを書いてControllerのアクションを叩くコードを書く経験をしました。このサイトでのJavaScriptはフレームワークに頼らず書いています。これから勉強を頑張りたい言語No.1です。ゆくゆくはTypeScriptを....'
-  );
-});
-
-js.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-// git
-
-const git = document.getElementById('GIT');
-
-git.addEventListener ('mouseenter', function () {
-  skill_description (
-    'git', 
-    'コミット、ブランチを切る、直前のコミットに戻す、比較する等の超基本はできます。ブランチは便利です。'
-  );
-});
-
-git.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//GitHub
-
-const github = document.getElementById('GitHub');
-
-github.addEventListener ('mouseenter', function () {
-  skill_description (
-    'GitHub', 
-    'ローカルからリモートにプッシュ、リモートからクローン、プルリク、マージする等の超基本はできます。毎日草生やすのを生き甲斐にしようと思っています。'
-  );
-});
-
-github.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Ruby
-
-const ruby = document.getElementById('Ruby');
-
-ruby.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Ruby', 
-    'Railsで使用する分にはある程度書けますが、Ruby単体でみるとまだまだ未熟者です。これからもっと勉強を深めて行きたいと考えております。'
-  );
-});
-
-ruby.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Rails
-
-const rails = document.getElementById('Rails');
-
-rails.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Rails', 
-    '一番自分の中では知っているフレームワークです。Railsのレールに乗っている分には作りやすいですが、レールから外れた瞬間自信がなくなります。後述の自作アプリぐらいのレベルなら作れます。'
-  );
-});
-
-rails.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//AWS
-
-const aws = document.getElementById('AWS');
-
-aws.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Amazon Web Services', 
-    'Railsを勉強始めた時の開発環境としてと、RISC!!という自作アプリをデプロイする時のインフラとして使用しました。デプロイは独学でやりきりました。'
-  );
-});
-
-aws.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Python
-
-const python = document.getElementById('Python');
-
-python.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Python', 
-    '初めて勉強した言語です。マイクラのブロック積み上げの自働化をするために勉強しました。その他にも企業分析の非常に簡単な対話形式のソフトを作りました。今後ラズパイと組み合わせて遊んでいきたいです。'
-  );
-});
-
-python.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Vim
-
-const vim = document.getElementById('Vim');
-
-vim.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Vim', 
-    'キーボードのみで完結するのは最強です。初心者ですが、大好きなエディターです。最近ではVim以外のソフトでカーソル移動を行う時にh,j,k,lで移動しようとする、保存の際に:wを押してしまうといった病気にかかっています。'
-  );
-});
-
-vim.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//MySQL
-
-const mysql = document.getElementById('MySQL');
-
-mysql.addEventListener ('mouseenter', function () {
-  skill_description (
-    'MySQL', 
-    'ググりながらならSQL文を書けます。RailsではActive RecordでSQL文を弄る機会がありませんでしたが、、理解を深める為、生のSQL文をかけるように学習しています。'
-  );
-});
-
-mysql.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//CAD
-
-const autocad = document.getElementById('AutoCAD');
-
-autocad.addEventListener ('mouseenter', function () {
-  skill_description (
-    'AutoCAD, 3DCAD', 
-    '簡単な部品なら2D,3Dどちらでも作図できます。現在の会社で、現場の作業効率を上げる為の部品を自分で設計し、3Dプリンターで制作しました。'
-  );
-});
-
-autocad.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//Lr
-
-const lr = document.getElementById('Lr');
-
-lr.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Lightroom', 
-    '写真が趣味で現像編集に使っています。特に星空写真や自然現象の写真を撮るのは大得意です。下の写真は雷を捉えた写真です。もちろんLightroomで現像しました。'
-  );
-  add_image ( 
-    './images/Thunder.jpg',
-    '雷'
-  );
-});
-
-lr.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//XD
-
-const xd = document.getElementById('XD');
-
-xd.addEventListener ('mouseenter', function () {
-  skill_description (
-    'Xd', 
-    '使い始めたばかりですが、このホームページはXdを使って大まかなデザインを設計しました。良い製品は良い設計からという言葉がよく身に染みました。'
-  );
-});
-
-xd.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//House keep
-
-const house_keeper = document.getElementById('House_Keeper');
-
-house_keeper.addEventListener ('mouseenter', function () {
-  skill_description (
-    '家事スキル', 
-    '多分同年代の中であれば結構上位に来る自信があります。料理から洗濯、パン作り、掃除、浴槽のカビ取りまで一通りなんでもできます。'
-  );
-});
-
-house_keeper.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-//English
-
-const uk = document.getElementById('UK');
-
-uk.addEventListener ('mouseenter', function () {
-  skill_description (
-    '英語', 
-    '実は英語できます。資格としてIELTS 6.0, TOEIC 795をもっています。幼少期のブラジル、中学3年間のシンガポール滞在、大学でのドイツ留学等、海外経験は豊富です。'
-  );
-});
-
-uk.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//German
-
-const ger = document.getElementById('GER');
-
-ger.addEventListener ('mouseenter', function () {
-  skill_description (
-    'ドイツ語', 
-    'ドイツ語もちょっとできます。資格としてはGoethe Zertifikat B1を取得しております。人生の目標であるアルプス制覇の為に学生時代から勉強しています。'
-  );
-});
-
-ger.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
-
-//port
-
-const por = document.getElementById('POR');
-
-por.addEventListener ('mouseenter', function () {
-  skill_description (
-    'ポルトガル語', 
-    'ポルトガル語もちょっとできます。レベルはA2ぐらいです。実はリスニングのみに関してではドイツ語よりもポルトガル語のほうが理解できます。'
-  );
-});
-
-por.addEventListener ('mouseleave', function () {
-  skill_description_delete ();
-});
-
 
 //add skill description with DOM control
 function skill_description (title, message) {
@@ -338,6 +33,131 @@ function skill_description_delete (id) {
   detail.textContent = null;
 }
 
+function add_event_to_skill (skill) {
+  const elem = document.getElementById(skill.id);
+
+  elem.addEventListener ('mouseenter', function () {
+    skill_description (
+      skill.title,
+      skill.message
+    );
+
+    if (skill.image) {
+      add_image(
+        skill.image.address,
+        skill.image.alt
+      );
+    }
+  });
+
+  elem.addEventListener ('mouseleave', function () {
+    skill_description_delete ();
+  });
+}
+
+const skills = [
+  {
+    id: 'HTML',
+    title: 'HTML',
+    message: 'このサイト程度であれば制作できます。気づけばdivだらけになっている気がしますが、、、このサイトはテンプレート等一切使わず０から全部自分で記述しました。'
+  },
+  {
+    id: 'CSS',
+    title: 'CSS',
+    message: '簡単なWebサイト程度であれば制作できます。このサイトはテンプレート等一切使わず０から全部自分で記述しました。ただしまだ未熟者の為、スパゲティコードになりかけてます。',
+  },
+  {
+    id: 'Bootstrap',
+    title: 'Bootstrap',
+    message: 'CSSに恐怖心を抱いていた頃は非常にお世話になりました。後述のRailsをつかった自作アプリ全てにおいて使用しております。',
+  },
+  {
+    id: 'JS',
+    title: 'JavaScript',
+    message: '後述の自作アプリで、Google Map APIを使いこなす必要があったため急遽一週間で詰め込み勉強しました。見よう見まねでRailsにjQueryで簡単なAjaxを書いてControllerのアクションを叩くコードを書く経験をしました。このサイトでのJavaScriptはフレームワークに頼らず書いています。これから勉強を頑張りたい言語No.1です。ゆくゆくはTypeScriptを....',
+  },
+  {
+    id: 'GIT',
+    title: 'git',
+    message: 'コミット、ブランチを切る、直前のコミットに戻す、比較する等の超基本はできます。ブランチは便利です。',
+  },
+  {
+    id: 'GitHub',
+    title: 'GitHub',
+    message: 'ローカルからリモートにプッシュ、リモートからクローン、プルリク、マージする等の超基本はできます。毎日草生やすのを生き甲斐にしようと思っています。',
+  },
+  {
+    id: 'Ruby',
+    title: 'Ruby',
+    message: 'Railsで使用する分にはある程度書けますが、Ruby単体でみるとまだまだ未熟者です。これからもっと勉強を深めて行きたいと考えております。',
+  },
+  {
+    id: 'Rails',
+    title: 'Rails',
+    message: '一番自分の中では知っているフレームワークです。Railsのレールに乗っている分には作りやすいですが、レールから外れた瞬間自信がなくなります。後述の自作アプリぐらいのレベルなら作れます。',
+  },
+  {
+    id: 'AWS',
+    title: 'Amazon Web Services',
+    message: 'Railsを勉強始めた時の開発環境としてと、RISC!!という自作アプリをデプロイする時のインフラとして使用しました。デプロイは独学でやりきりました。',
+  },
+  {
+    id: 'Python',
+    title: 'Python', 
+    message: '初めて勉強した言語です。マイクラのブロック積み上げの自働化をするために勉強しました。その他にも企業分析の非常に簡単な対話形式のソフトを作りました。今後ラズパイと組み合わせて遊んでいきたいです。',
+  },
+  {
+    id: 'Vim',
+    title: 'Vim',
+    message: 'キーボードのみで完結するのは最強です。初心者ですが、大好きなエディターです。最近ではVim以外のソフトでカーソル移動を行う時にh,j,k,lで移動しようとする、保存の際に:wを押してしまうといった病気にかかっています。',
+  },
+  {
+    id: 'MySQL',
+    title: 'MySQL',
+    message: 'ググりながらならSQL文を書けます。RailsではActive RecordでSQL文を弄る機会がありませんでしたが、、理解を深める為、生のSQL文をかけるように学習しています。',
+  },
+  {
+    id: 'AutoCAD',
+    title: 'AutoCAD, 3DCAD',
+    message: '簡単な部品なら2D,3Dどちらでも作図できます。現在の会社で、現場の作業効率を上げる為の部品を自分で設計し、3Dプリンターで制作しました。',
+  },
+  {
+    id: 'Lr',
+    title: 'Lightroom',
+    message: '写真が趣味で現像編集に使っています。特に星空写真や自然現象の写真を撮るのは大得意です。下の写真は雷を捉えた写真です。もちろんLightroomで現像しました。',
+    image: {
+      address: './images/Thunder.jpg',
+      alt: '雷',
+    }
+  },
+  {
+    id: 'XD',
+    title: 'Xd',
+    message: '使い始めたばかりですが、このホームページはXdを使って大まかなデザインを設計しました。良い製品は良い設計からという言葉がよく身に染みました。',
+  },
+  {
+    id: 'House_Keeper',
+    title: '家事スキル',
+    message: '多分同年代の中であれば結構上位に来る自信があります。料理から洗濯、パン作り、掃除、浴槽のカビ取りまで一通りなんでもできます。',
+  },
+  {
+    id: 'UK',
+    title: '英語',
+    message: '実は英語できます。資格としてIELTS 6.0, TOEIC 795をもっています。幼少期のブラジル、中学3年間のシンガポール滞在、大学でのドイツ留学等、海外経験は豊富です。',
+  },
+  {
+    id: 'GER',
+    title: 'ドイツ語',
+    message: 'ドイツ語もちょっとできます。資格としてはGoethe Zertifikat B1を取得しております。人生の目標であるアルプス制覇の為に学生時代から勉強しています。',
+  },
+  {
+    id: 'POR',
+    title: 'ポルトガル語',
+    message: 'ポルトガル語もちょっとできます。レベルはA2ぐらいです。実はリスニングのみに関してではドイツ語よりもポルトガル語のほうが理解できます。',
+  },
+];
+
+skills.forEach(add_event_to_skill);
 
 //--works javascript-----------------------------------------------------
 
